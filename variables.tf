2fa17b97248d3b32bc52592ced5d0fe5bbc4bfcd
ChrisIns <inselin.christopher@gmail.com>
2022-10-10 07:48:18 -0400

    Adding variables.tf and Terraform tfvars file and updated main file

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "images" {
+	type = list(string)
+}
+
+variable "path" {
+	type = list(string)
+}
+
+variable "container_path" {
+	type = list(string)
+}
+
+variable "network_name" {
+	type = string
+}
+
+variable "ports_internal" {
+	type = list(number)
+}
+
+variable "ports_external" {
+	type = list(number)
+}
+
+variable "ip" {
+	type = string
+	description = "IP localhost"
+}
+
+variable "host_path" {
+	type = string
+}
+
+variable "volume_name" {
+	type = string
+}
+
+variable "mysql_pass" {
+	type = string
+}
+
+variable "mysql_database" {
+	type = string
+}
